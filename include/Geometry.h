c0a2594118b53dfeb1fcacd74c86b36bf1ee39c5
Aurel Gruber <aurel.gruber@infix.ch>
2017-09-24 19:10:39 +0200

    added copyright & license notice

--- include/Geometry.h
+++ include/Geometry.h
@@ -1,6 +1,25 @@
-//
-// Created by Aurel Gruber on 13.09.17.
-//
+/**
+ * Copyright 2017, infix development gmbh <admin@infix.ch>
+ *
+ * This file is part of the Geometry Processing Pipeline (GPP) Library.
+ *
+ * The GPP Library is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The GPP Library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the GPP Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license LGPL-3.0+
+ *
+ * Author: Aurel Gruber on 12.09.17.
+ */
 
 #ifndef GEOMETRYPROCESSINGPIPELINE_GEOMETRY_H
 #define GEOMETRYPROCESSINGPIPELINE_GEOMETRY_H
